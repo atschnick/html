e7b0b58080d0c1b5676ba34c80fd43944bfcc23b
ANDy Schnick <atschnick@gmail.com>
2025-01-24 16:00:40 -0600

    Updated the Splash & Background images

---
+++ in_dev/main.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<?php include 'head.php'; ?>
+<body class=parallax>
+<?php
+        /*require_once 'private/login_auth_thai_village.php';
+
+        $dbserver = mysqli_connect($dbhostname, $dbusername, $dbpassword, $dbdatabase);
+        if(!$dbserver) die ("Unable to connect to MySQL: " . mysqli_connect_error());
+
+	mysqli_close($dbserver);*/
+?>
+<center>
+        <table>
+                <tr>
+                        <td><center><h1>Welcome to ANDy Schnick's Website</h1></center></td>
+                </tr>
+                <tr>
+                        <td><p></p></td>
+                </tr>
+                <tr>
+                        <td>
+                                <center>
+                                        <a class=button href=character_connections.php>Character Connections</a>
+                                        <a class=button href=all_sales_thai_village.php>Thai Village</a>
+                                </center>
+                        </td>
+                </tr>
+        </table>
+</center>
+</body>
+</html>

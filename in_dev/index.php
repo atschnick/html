e8e599bc2459b6b30a4da1f81a0d4a25c8067f66
ANDy Schnick <atschnick@gmail.com>
2025-02-13 13:54:01 -0600

    New Splash Page In Development

--- in_dev/index.php
+++ in_dev/index.php
@@ -1,6 +1,11 @@
 <!DOCTYPE html>
 <?php include "head.php" ?>
 <body>
-<h1>Hello World!  This is ANDy Schnick!</h1>
+<div class="card">
+	<center><img src=images/me.jpg height=200 width=150>
+	<h1>Welcome to ANDy Schnick's Website</h1>
+	<br>
+	<a class="splashbutton" href="">Continue</a></center>
+</div>
 </body>
 </html>

a5dd6762f79110c3fdcd1d6c66b94db409252c8d
ANDy Schnick <atschnick@gmail.com>
2025-02-14 20:36:37 -0600

    Added a card container so it doesn't mess up my gradient animation.

--- in_dev/index.php
+++ in_dev/index.php
@@ -1,11 +1,13 @@
 <!DOCTYPE html>
 <?php include "head.php" ?>
 <body>
-<div class="card">
-	<center><img src=images/me.jpg height=200 width=150>
-	<h1>Welcome to ANDy Schnick's Website</h1>
-	<br>
-	<a class="splashbutton" href="">Continue</a></center>
+<div class="card_container">
+	<div class="card">
+		<center><img src=images/me.jpg height=200 width=150>
+		<h1>Welcome to ANDy Schnick's Website</h1>
+		<br>
+		<a class="splashbutton" href="">Continue</a></center>
+	</div>
 </div>
 </body>
 </html>
